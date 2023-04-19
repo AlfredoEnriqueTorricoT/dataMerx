495e135ad9c0264a2bcb274bd020c1bb2c9a0e94
AlexanderTorrico <torrico.torrico.alexander@gmail.com>
2023-04-18 20:17:36 -0400

    delete folders trash

--- frontend/src/store/sims/actionTypes.js
+++
@@ -1,8 +0,0 @@
-/* USERS */
-export const GET_SIMS = "GET_SIMS"
-export const GET_SIMS_DISPONIBLES = "GET_SIMS_DISPONIBLES"
-export const INSERT_SIM = "INSERT_SIM"
-export const EDIT_SIM = "EDIT_SIM"
-export const WAIT_SIMS = "WAIT_RESPONSE"
-export const GET_SIMS_SUCCESS = "GET_SIMS_SUCCESS"
-export const GET_SIMS_FAIL = "GET_SIMS_FAIL"
